6c6961a6e4cbb00706b6c2924b5c426e9afbd836
peace0phmind <peace0phmind@gmail.com>
2020-09-20 16:23:24 +0800

    rm code

--- cmdutils.c
+++ cmdutils.c
@@ -34,7 +34,6 @@
 #include "libavformat/avformat.h"
 #include "libavfilter/avfilter.h"
 #include "libavdevice/avdevice.h"
-//#include "libavresample/avresample.h"
 #include "libswscale/swscale.h"
 #include "libswresample/swresample.h"
 #include "libpostproc/postprocess.h"
