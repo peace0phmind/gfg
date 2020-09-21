f89eb3dd606c097c9fcdd109430007eeed9dda2e
peace0phmind <peace0phmind@gmail.com>
2020-09-21 17:30:24 +0800

    add rtsp tcp

---
+++ examples/multiple_rtsp_fps.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/peace0phmind/gfg"
+	"time"
+)
+
+func main() {
+	g0 := gfg.NewGfg("-rtsp_transport tcp -i rtsp://admin:Zyx123456@192.168.1.10 -vf fps=fps=1/5 test001_%06d.jpeg", true, nil)
+	go g0.Run()
+
+	g1 := gfg.NewGfg("-rtsp_transport tcp -i rtsp://admin:Zyx123456@192.168.1.11 -vf fps=fps=1/10 test002_%06d.jpeg", true, nil)
+	go g1.Run()
+
+	time.Sleep(4 * time.Hour)
+
+	g0.Stop()
+	g1.Stop()
+}
