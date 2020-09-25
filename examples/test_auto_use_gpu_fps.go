240e6d026786ddc841a2ed551383b9503d4c007d
peace0phmind <peace0phmind@gmail.com>
2020-09-25 17:12:28 +0800

    add auto gpu test case

--- examples/test_auto_use_gpu_fps.go
+++ examples/test_auto_use_gpu_fps.go
@@ -6,8 +6,20 @@ import (
 	"time"
 )
 
+type AutoGpuCallBackTest struct {
+}
+
+func (c *AutoGpuCallBackTest) WritePacket(pkt *gfg.Packet) {
+	log.Printf("get packet size: %d\n", pkt.Size())
+}
+
 func main() {
 	g := gfg.NewGfg("-rtsp_transport tcp -i rtsp://admin:Zyx123456@192.168.1.10 -vf fps=fps=1/5 test001_%06d.jpeg")
+
+	g.SetAutoUseGpu(true)
+	g.SetWritePacket(false)
+	g.SetCallback(&AutoGpuCallBackTest{})
+
 	go g.Run()
 
 	time.Sleep(5 * time.Second)
